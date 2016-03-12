a9e4d1f0fe91bd2ad99adfdf5539f0efffe6fe3f
JamStop <jimmyyue1@gmail.com>
2016-03-12 12:16:56 -0800

    storyboard + views setup

---
+++ SoundParticles/View Controllers/ParticleViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ParticleViewController.swift
+//  SoundParticles
+//
+//  Created by Jimmy Yue on 3/12/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import UIKit
+
+class ParticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ SoundParticles/Views/ParticleView.swift
@@ -0,0 +1,13 @@
+//
+//  ParticleView.swift
+//  SoundParticles
+//
+//  Created by Jimmy Yue on 3/12/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import Cocoa
+
+class ParticleView: UIView {
+
+}
