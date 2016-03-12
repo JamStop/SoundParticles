e617169967fefd0e252431afd4d4aa59def89e99
JamStop <jimmyyue1@gmail.com>
2016-03-12 12:07:03 -0800

    particle initialization

---
+++ SoundParticles/Views/ParticleScene.swift
@@ -0,0 +1,14 @@
+//
+//  ParticleScene.swift
+//  SoundParticles
+//
+//  Created by Jimmy Yue on 3/12/16.
+//  Copyright © 2016 Jimmy Yue. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ParticleScene: SKScene {
+
+}
